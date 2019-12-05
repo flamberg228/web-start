95b1b6e3642865256a45a312bf0a2ac0ebff3995
flamberg228 <58427945+flamberg228@users.noreply.github.com>
2019-12-05 18:40:34 +0300

    refact: add scss files in gulpfile

--- project_gulp/gulpfile.js
+++ project_gulp/gulpfile.js
@@ -11,10 +11,11 @@ function bs() {
   });
   watch("./*.html").on('change', browserSync.reload);
   watch("./sass/**/*.sass").on('change', serveSass);
+  watch("./sass/**/*.scss").on('change', serveSass);
   watch("./js/*.js").on('change', browserSync.reload);
 };
 function serveSass() {
-  return src("app/sass/*.scss")
+  return src("./sass/*.sass", "./sass/**/*.scss")
       .pipe(sass())
       .pipe(dest("./css"))
       .pipe(browserSync.stream());
